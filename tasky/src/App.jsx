77b8ce9fd09e05c2cf870c0e3b6fc5708f5007d3
Mark Connolly Doyle <markjames2000@hotmail.com>
2026-09-24 19:40:48 +0100

    Add task functionality

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -2,11 +2,12 @@ import './App.css';
 import Task from './components/Task';
 import { useState } from 'react';
 import AddTaskForm from './components/Form';
+import { v4 as uuidv4 } from 'uuid';
 
 
 function App() {
 
-      const [ taskState, setTaskState ] = useState({
+  const [ taskState, setTaskState ] = useState({
     tasks: [
       { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", priority: "Medium", done: false },
       { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", priority: "High", done: false },
@@ -25,25 +26,64 @@ function App() {
     const tasks = [...taskState.tasks];
     tasks.splice(taskIndex, 1);
     setTaskState({tasks});
-  } 
+  }
+
+  const [ formState, setFormState ] = useState({
+      title: "",
+      description: "",
+      deadline: ""
+  });
+
+  const formChangeHandler = (event) => {
+  let form = {...formState};
+
+  switch(event.target.name) {
+    case "title":
+        form.title = event.target.value;
+        break;
+    case "description":
+        form.description = event.target.value;
+        break;
+    case "deadline":
+        form.deadline = event.target.value;
+        break;
+    default:
+        form = formState;
+  }
+  setFormState(form);
+  }
+
+  console.log(formState);
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    const tasks = [...taskState.tasks];
+    const form = {...formState};
+
+    form.id = uuidv4();
+    
+    tasks.push(form);
+    setTaskState({tasks});
+  }
 
 
     return (
       <div className="container">
         <h1>Tasky</h1>
-          {taskState.tasks.map((task, index) => (              
-            <Task 
-              title={task.title}
-              description={task.description}
-              deadline={task.deadline}
-              priority={task.priority}
-              key={task.id}
-              done={task.done}
-              markDone={() => doneHandler(index)}
-              deleteTask = {() => deleteHandler(index)}
-            />
-          ))} 
-          <AddTaskForm />
+        {taskState.tasks.map((task, index) => (              
+          <Task 
+            title={task.title}
+            description={task.description}
+            deadline={task.deadline}
+            priority={task.priority}
+            key={task.id}
+            done={task.done}
+            markDone={() => doneHandler(index)}
+            deleteTask = {() => deleteHandler(index)}
+          />
+        ))} 
+        <AddTaskForm submit={formSubmitHandler} change={formChangeHandler} />
       </div>
     );
 
